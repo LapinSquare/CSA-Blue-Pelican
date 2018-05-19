453f847b7f59d518033445f3aab15895994c58a8
LapinSquare <citron.huynh@gmail.com>
2018-05-19 14:12:49 -0600

    Upload Celebrity Code

---
+++ L03 String Ops/NameThatCelebrity.java
@@ -0,0 +1,19 @@
+/** 
+ * A Blue Pelican project. 
+ * @author Kim Huynh
+ * @version 8/31/2017
+ */
+ 
+public class NameThatCelebrity {
+
+	public static void main(String[] args) {
+		String s1 = "Allan Alda";
+		String s2 = "John Wayne";
+		String s3 = "Gregory Peck";  
+		System.out.println(s1+">>>"+s1.substring(2,7)); 
+		System.out.println(s2+">>>"+s2.substring(2,7)); 
+		System.out.println(s3+">>>"+s3.substring(2,9));
+
+	}
+
+}
