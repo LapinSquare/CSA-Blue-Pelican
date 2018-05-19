c662797eea6c97a8598947bb14b9eeca809800cd
LapinSquare <citron.huynh@gmail.com>
2018-05-19 14:49:47 -0600

    ByHalves Upload

---
+++ L11 For Loops/ByHalves.java
@@ -0,0 +1,18 @@
+/** 
+ * Simple activity
+ * @author Kim Huynh 
+ * @version 11/10/2017
+ */
+ 
+public class ByHalves {
+
+	public static void main(String[] args) {
+		System.out.println("Here we go!"); 
+		System.out.println(""); 
+		for (double i= -10.0 ; i<=10 ; i+=0.5) { 
+			System.out.println(i);
+		}
+
+	}
+
+}
