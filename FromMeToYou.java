a6016058e44735ce7780bd0d3c3cdcfe52eb45ff
LapinSquare <citron.huynh@gmail.com>
2018-05-19 13:58:06 -0600

    FromMeToYou.java creation
    Two files down!

---
+++ FromMeToYou.java
@@ -0,0 +1,20 @@
+/** 
+ * A Blue Pelican project. A letter. To whom? Who knows.
+ * @author Kim Huynh
+ * @version 08/29/17
+ */
+ 
+public class FromMeToYou {
+
+	public static void main(String[] args) {
+		System.out.println("From: Bill Smith"); 
+		System.out.println("Address: Dell Computer, Bldg 13"); 
+		System.out.println("Date: April 12, 2005"); 
+		System.out.println(""); 
+		System.out.println("To: Jack Jones"); 
+		System.out.println(""); 
+		System.out.println("Message: Help! I'm trapped inside a computer!");
+
+	}
+
+}
