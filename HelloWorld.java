1b0e02188ee4db1608c65d8ed7a0610072284564
LapinSquare <citron.huynh@gmail.com>
2018-05-19 13:54:00 -0600

    HelloWorld.java creation
    One file done!

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+/** 
+ * Printing "Hello World" to console...for the millionth time. 
+ * Involves other misc assignments with it.
+ * @author Kim Huynh
+ * @version 08/28/17
+ */
+ 
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!"); 
+		System.out.println("My name is Kim."); 
+		System.out.println("I've currently been fascinated by Mili.");   
+		System.out.println("I don't have a particular song I favor. Perhaps Space Colony or world.execute(me).");
+		System.out.println("The lyrics? I surely don't remember such things.");  
+		System.out.println("================================================================");
+		System.out.println("| The difference between println and print is...               |"); 
+		System.out.println("| ...print makes the next statement go on the same line.       |"); 
+		System.out.println("================================================================"); 
+		System.out.println("Would you like some random numbers by the way?"); 
+		System.out.println(25/5); 
+		System.out.println(13 * 159.56);
+
+	}
+
+}
