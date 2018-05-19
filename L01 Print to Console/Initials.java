9e812b62617873c91c1685336c5bf717d1fa754d
LapinSquare <citron.huynh@gmail.com>
2018-05-19 14:04:42 -0600

    Initials.java creation
    Now you know my initials.

---
+++ L01 Print to Console/Initials.java
@@ -0,0 +1,19 @@
+/** 
+ * Making simple digital printout.
+ * @author Kim Huynh
+ * @version 08/28/17
+ */
+ 
+public class Initials {
+
+	public static void main(String[] args) {
+		System.out.println("KK    KK  H    H H    H  "); 
+		System.out.println("KK  KK    H    H H    H"); 
+		System.out.println("KKK       HHHHHH HHHHHH  "); 
+		System.out.println("KK KK     H    H H    H  "); 
+		System.out.println("KK   KK   H    H H    H "); 
+		System.out.println("KK     KK H    H H    H ");
+
+	}
+
+}
